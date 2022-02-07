49b9c14d8ffda1ded6d89d1f0b0bcf2cbb692dd2
unknown <hda.karima@gmail.com>
2022-02-07 23:04:20 +0100

    testcontroller

--- src/test/java/com/example/booksapi/controller/BookControllerTests.java
+++ src/test/java/com/example/booksapi/controller/BookControllerTests.java
@@ -1,5 +1,45 @@
 package com.example.booksapi.controller;
 
+import com.example.booksapi.model.Book;
+import com.example.booksapi.model.Commentaire;
+import com.example.booksapi.repository.BookRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@WebMvcTest
 public class BookControllerTests {
+
+     private BookController bookcontroller=new BookController();
+    @MockBean BookRepository bookrepository;
+    @Test
+    void getBooksTest(){
+        List<Commentaire> commentaires = new ArrayList<>();
+        commentaires.add(new Commentaire(10,"really good reading"));
+        when(bookrepository.findAll()).thenReturn((List<Book>) Stream.of(
+                new Book(1,"la boite a merveilles",commentaires )));
+        assertEquals(1,bookcontroller.getBooks().size());
+
+    }
+//Test pour enregistrer un livre
+    @Test
+    public void saveBookTest() {
+        List<Commentaire> commentaires = new ArrayList<>();
+        commentaires.add(new Commentaire(10,"really good reading"));
+        Book book = new Book(9, "Red Book",commentaires);
+        when(bookrepository.save(book)).thenReturn(book);
+        assertEquals(book, bookcontroller.addBook(book));
+    }
 
 }
